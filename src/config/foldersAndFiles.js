4e05e6b15f0155c11fdf57e621e1396fb7137571
panwrona <mariusz.brona@gmail.com>
2020-02-24 12:38:35 +0100

    Remove missing files

--- src/config/foldersAndFiles.js
+++ src/config/foldersAndFiles.js
@@ -7,15 +7,11 @@ export function foldersAndFiles(currentAppName, newName) {
 
   return [
     `ios/${nS_CurrentAppName}`,
-    `ios/${nS_CurrentAppName}-tvOS`,
-    `ios/${nS_CurrentAppName}-tvOSTests`,
     `ios/${nS_CurrentAppName}.xcodeproj`,
-    `ios/${nS_NewName}.xcodeproj/xcshareddata/xcschemes/${nS_CurrentAppName}-tvOS.xcscheme`,
     `ios/${nS_NewName}.xcodeproj/xcshareddata/xcschemes/${nS_CurrentAppName}.xcscheme`,
     `ios/${nS_CurrentAppName}Tests`,
     `ios/${nS_NewName}Tests/${nS_CurrentAppName}Tests.m`,
     `ios/${nS_CurrentAppName}.xcworkspace`,
-    `ios/${nS_NewName}/${nS_CurrentAppName}.entitlements`,
     `ios/${nS_NewName}-Bridging-Header.h`,
   ];
 }
