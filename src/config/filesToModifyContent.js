84500e1b8e1956e5116e81475f49a6642e735df0
panwrona <mariusz.brona@gmail.com>
2020-04-09 12:10:07 +0200

    Update paths

--- src/config/filesToModifyContent.js
+++ src/config/filesToModifyContent.js
@@ -1,5 +1,6 @@
 // nS - No Space
 // lC - Lowercase
+import path from 'path';
 
 export function filesToModifyContent(currentAppName, newName) {
   const nS_CurrentAppName = currentAppName.replace(/\s/g, '');
@@ -18,26 +19,26 @@ export function filesToModifyContent(currentAppName, newName) {
         'index.js',
         'index.android.js',
         'index.ios.js',
-        `ios/${nS_NewName}.xcodeproj/project.pbxproj`,
-        `ios/${nS_NewName}.xcworkspace/contents.xcworkspacedata`,
-        `ios/${nS_NewName}.xcodeproj/xcshareddata/xcschemes/${nS_NewName}.xcscheme`,
-        `ios/${nS_NewName}/AppDelegate.m`,
-        'android/settings.gradle',
-        `ios/${nS_NewName}Tests/${nS_NewName}Tests.m`,
-        'ios/build/info.plist',
-        'ios/Podfile',
+        path.join('ios', `${nS_NewName}.xcodeproj`, 'project.pbxproj'),
+        path.join('ios', `${nS_NewName}.xcworkspace`, 'contents.xcworkspacedata'),
+        path.join('ios', `${nS_NewName}.xcodeproj`, 'xcshareddata', 'xcschemes', `${nS_NewName}.xcscheme`),
+        path.join('ios', `${nS_NewName}`, 'AppDelegate.m'),
+        path.join('android', 'settings.gradle'),
+        path.join('ios', `${nS_NewName}Tests`, `${nS_NewName}Tests.m`),
+        path.join('ios', 'build', 'info.plist'),
+        path.join('ios', 'Podfile'),
         'app.json',
       ],
     },
     {
       regex: `text="${currentAppName}"`,
       replacement: `text="${newName}"`,
-      paths: [`ios/${nS_NewName}/Base.lproj/LaunchScreen.xib`],
+      paths: [path.join('ios', `${nS_NewName}`, 'Base.lproj', 'LaunchScreen.xib')],
     },
     {
       regex: currentAppName,
       replacement: newName,
-      paths: [`ios/${nS_NewName}/Info.plist`],
+      paths: [path.join('ios', `${nS_NewName}`, 'Info.plist')],
     },
     {
       regex: `"name": "${nS_CurrentAppName}"`,
